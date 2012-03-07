6fe171bf8c8fee8477ae7e82b3902082777d9989
Bernhard Häussner <bxt@die-optimisten.net>
2012-03-07 22:07:57 +0100

    Added some (now working) scripting

--- script.js
+++ script.js
@@ -1,59 +1,132 @@
 (function() {
-  function updateTime() {
-    var now = new Date(),
+  "use strict";
+  
+  
+  var updateInterval;
+  var ffUpdateInterval;
+  
+  function updateTime(e,now) {
+    now = now || new Date();
+    var i,l,
       hour = now.getHours(), 
       minute = now.getMinutes(), 
-      words, twelveHourClockHour,
-      i, len, highlights = [],
       nums = ["one", "two", "three", "four", "five", "six",
-              "seven", "eight", "nine", "ten", "eleven", "twelve"];
+              "seven", "eight", "nine", "ten", "eleven", "twelve",
+              "nearly", "to", "past", "three1", "quarter", "half",
+              "sixSeven", "twelveFive", "tenNine", "twelveFive", 
+              "oneS", "quarter2", "o-clock"
+             ],
+      numState = {},
+      highlights = [];
 
-    if (minute >= 3 && minute < 7) {
-      highlights.push("minute-five", "past");
-    } else if (minute >= 7 && minute < 13) {
-      highlights.push("minute-ten", "past");
-    } else if (minute >= 13 && minute < 17) {
-      highlights.push("quarter", "past");
-    } else if (minute >= 17 && minute < 23) {
-      highlights.push("twenty", "past");
-    } else if (minute >= 23 && minute < 37) {
-      highlights.push("half", "past");
-    } else if (minute >= 37 && minute < 42) {
-      highlights.push("twenty", "to");
-    } else if (minute >= 42 && minute < 48) {
-      highlights.push("quarter", "to");
+    if (minute >= 4 && minute < 8) {
+      highlights.push("nearly", "past");
+    } else if (minute >= 8 && minute < 13) {
+      highlights.push("nearly", "to", "quarter");
+    } else if (minute >= 13 && minute < 18) {
+      highlights.push("quarter");
+    } else if (minute >= 18 && minute < 23) {
+      highlights.push("nearly", "past", "quarter");
+    } else if (minute >= 23 && minute < 27) {
+      highlights.push("nearly", "to", "half");
+    } else if (minute >= 27 && minute < 34) {
+      highlights.push("half");
+    } else if (minute >= 34 && minute < 38) {
+      highlights.push("nearly", "past", "half");
+    } else if (minute >= 38 && minute < 43) {
+      highlights.push("nearly", "to", "three1", "quarter");
+    } else if (minute >= 43 && minute < 48) {
+      highlights.push("three1", "quarter");
     } else if (minute >= 48 && minute < 53) {
-      highlights.push("minute-ten", "to");
+      highlights.push("nearly", "past", "three1", "quarter");
     } else if (minute >= 53 && minute < 57) {
-      highlights.push("minute-five", "to");
+      highlights.push("nearly", "to");
     } else {
       highlights.push("o-clock");
     }
     
-    twelveHourClockHour = (hour % 12 !== 0) ? hour % 12 : 12;
+    var twelveHourClockHour = (hour % 12 !== 0) ? hour % 12 : 12;
     
-    if (twelveHourClockHour === 12 && minute >= 37) {
-      highlights.push(nums[0]);
+    if (twelveHourClockHour === 12 && minute >= 8) {
+      highlights.push("one");
     } else {
-      highlights.push((minute < 37) ? nums[twelveHourClockHour-1] : nums[twelveHourClockHour]);
+      highlights.push((minute < 8) ? nums[twelveHourClockHour-1] : nums[twelveHourClockHour]);
     }
     
-    words = document.querySelectorAll(".word");
-    len = words.length;
-    for (i = 0; i < len; i++) {
-      words[i].className = "word";
+    for (i = 0, l = highlights.length; i < l; i++) {
+      numState[highlights[i]] = true;
     }
     
-    len = highlights.length;
-    for (i = 0; i < len; i++) {
-      document.getElementById(highlights[i]).className = "word active";
+    if(numState["one"] && !numState["o-clock"])
+      numState["oneS"] = true;
+    
+    if(numState["quarter"])
+      numState["quarter2"] = true;
+    
+    if(numState["three"] && numState["o-clock"]) {
+      numState["three1"] = true;
+      numState["three"] = false;
     }
-  };
+    
+    if(numState["four"] && numState["o-clock"]) {
+      numState["quarter"] = true;
+      numState["four"] = false;
+    }
+    
+    if(numState["twelve"] || numState["five"])
+      numState["twelveFive"] = true;
+    
+    if(numState["ten"] || numState["nine"])
+      numState["tenNine"] = true;
+    
+    if(numState["six"] || numState["seven"])
+      numState["sixSeven"] = true;
+    
+    
+    
+    for (i = 0, l = nums.length; i < l; i++) {
+      document.getElementById(nums[i]).className =
+        numState[nums[i]] ? "word active" : "word";
+    }
+    
+  }
   
   function ready() {
-    var updateInterval = setInterval(updateTime, 1000);
     updateTime();
-  };
+    updateInterval = setInterval(updateTime, 1000);
+  }
   
+  function fastForward() {
+    if(ffUpdateInterval) {
+      window.clearInterval(ffUpdateInterval);
+      ready();
+      return false;
+    }
+    
+    var now=new Date(),
+        i=now.getMinutes(),
+        k=now.getHours()%12;
+    
+    window.clearInterval(updateInterval);
+    
+    ffUpdateInterval = window.setInterval(function(){
+      updateTime(null, new Date(2012,6,3,k,i));
+      
+      i++;
+      if(i >= 60) { k++; i = 0; }
+      if(k >= 12) { k = 0; }
+    },250);
+    return false;
+  }
+  
+  function resize() {
+    document.getElementById("qlock").style.marginTop = 
+      Math.max(window.innerHeight-464,0)/2+50+"px";
+  }
+  
+  document.getElementById("ff").addEventListener('click', fastForward);
   window.addEventListener('load', ready);
+  window.addEventListener('resize', resize);
+  resize();
+  
 })();
